e1988d1d48f65df5d22691ad2115f52853feb70f
bygalacos <57764369+bygalacos@users.noreply.github.com>
2022-12-22 00:33:15 +0300

    Add files via upload

--- zabbix_agent_5.2.ps1
+++ zabbix_agent_5.2.ps1
@@ -7,7 +7,7 @@
 # Grant Administrator Privileges
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" `"$args`"" -Verb RunAs; exit
+   Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" `"$args`"" -Verb RunAs; exit
 }
 else 
 {
