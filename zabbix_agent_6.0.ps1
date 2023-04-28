c89df2cecc1af543ce35bdf1e618fdd1193ccf25
bygalacos <57764369+bygalacos@users.noreply.github.com>
2023-04-28 17:22:50 +0300

    Add files via upload

---
+++ zabbix_agent_6.0.ps1
@@ -0,0 +1,96 @@
+﻿#  Version:        1.0
+#  Author:         bygalacos
+#  Github:         github.com/bygalacos
+#  Creation Date:  22.12.2022
+#  Purpose/Change: Initial script development
+
+# Grant Administrator Privileges
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+   Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" `"$args`"" -Verb RunAs; exit
+}
+else 
+{
+   Write-Host "`nAdministrator Privileges granted, continuing...`n" -ForegroundColor Green
+}
+
+# Check If Zabbix Agent Running
+if (Get-WmiObject -class Win32_Process -Filter 'Name="zabbix_agentd.exe"' -ErrorAction SilentlyContinue)
+{ 
+   $pathZabbixExe = (Get-WmiObject -class Win32_Process -Filter 'Name="zabbix_agentd.exe"').path
+   $pathZabbixFolder = (Get-WmiObject -class Win32_Process -Filter 'Name="zabbix_agentd.exe"').path.SubString(0, (Get-WmiObject -class Win32_Process -Filter 'Name="zabbix_agentd.exe"').path.LastIndexOf('\')) | Split-Path
+   Write-Host "`n[Zabbix Agent] already running, uninstalling...`n"
+   Start-Sleep -Seconds 1
+   & $pathZabbixExe --config $pathZabbixFolder\conf\zabbix_agentd.conf --stop
+   Start-Sleep -Seconds 1
+   & $pathZabbixExe --config $pathZabbixFolder\conf\zabbix_agentd.conf --uninstall
+   Start-Sleep -Seconds 3
+   Remove-Item $pathZabbixFolder -Force -Recurse
+   if (Test-Path -Path C:\zabbix_agentd.log) {
+       $logdate = Get-Date -Format "dd.MM.yyyy_HH.mm"
+       Rename-Item -Path "C:\zabbix_agentd.log" -NewName "C:\zabbix_agentd_$logdate.log"
+       Write-Host "`n[Zabbix Agent] Log renamed successfully`n"
+   }
+   Write-Host "`n[Zabbix Agent] removed successfully`n" -ForegroundColor Green
+}
+else 
+{
+   Write-Host "`nUnable to find running [Zabbix Agent], continuing...`n"
+}
+
+# Download & Install Zabbix Agent Files
+Start-Sleep -Seconds 3
+Write-Host "`n[Zabbix Agent] initializing installation`n"
+if (Test-Path -Path C:\zabbix_agent) {
+    Remove-Item C:\zabbix_agent -Force -Recurse
+}
+if (Test-Path -Path C:\zabbix_agentd.log) {
+    Remove-Item C:\zabbix_agentd.log -Force
+}
+Start-Sleep -Seconds 1
+$zipUrl = "https://cdn.zabbix.com/zabbix/binaries/stable/6.0/6.0.17/zabbix_agent-6.0.17-windows-amd64.zip"
+$extractPath = "C:\zabbix_agent"
+Invoke-WebRequest -Uri $zipUrl -OutFile "$env:TEMP\zabbix_agent.zip"
+Expand-Archive -Path "$env:TEMP\zabbix_agent.zip" -DestinationPath $extractPath
+Remove-Item "$env:TEMP\zabbix_agent.zip" -Force
+Write-Host "`n[Zabbix Agent] files installed successfully`n" -ForegroundColor Green
+Start-Sleep -Seconds 3
+
+# Set Variables
+$confFile = "C:\zabbix_agent\conf\zabbix_agentd.conf"
+$hostname = hostname.exe
+
+# Check if arguments were provided if not ask user to input
+if ($args) {
+    $zabbixIP = $args[0]
+} else {
+    $zabbixIP = Read-Host 'Enter the IP address of the Zabbix server'
+}
+
+# Change Value (LF)
+(Get-Content $confFile) -join "`n" -replace "Server=127.0.0.1", "Server=$zabbixIP" | Set-Content $confFile
+(Get-Content $confFile) -join "`n" -replace "ServerActive=127.0.0.1", "ServerActive=$zabbixIP" | Set-Content $confFile
+(Get-Content $confFile) -join "`n" -replace "Hostname=Windows host", "Hostname=$hostname" | Set-Content $confFile
+
+# Install & Run
+Start-Sleep -Seconds 1
+& C:\zabbix_agent\bin\zabbix_agentd.exe --config C:\zabbix_agent\conf\zabbix_agentd.conf --install
+Start-Sleep -Seconds 1
+& C:\zabbix_agent\bin\zabbix_agentd.exe --config C:\zabbix_agent\conf\zabbix_agentd.conf --start
+Start-Sleep -Seconds 1
+$zabbixTime = (Get-Date (Get-Process zabbix_agentd).StartTime)
+Write-Host "`n[Zabbix Agent] started at -> $zabbixTime`n" -ForegroundColor Green
+Start-Sleep -Seconds 1
+
+# Script Cleanup
+Write-Host "`n[Zabbix Agent] installation script leftovers removing...`n"
+Start-Sleep -Seconds 3
+Remove-Item $MyInvocation.MyCommand.Source
+Write-Host "`n[Zabbix Agent] installation script leftovers removed successfully`n" -ForegroundColor Green
+Write-Host "`nMade with ♥ by bygalacos`n" -ForegroundColor Yellow
+Write-Host "`nhttps://github.com/bygalacos`n" -ForegroundColor Yellow
+Start-Sleep 3
+Write-Host "`n!!!WARNING!!!`n" -ForegroundColor Red
+Write-Host "`nThis session will self destruct in 5 seconds...`n" -ForegroundColor Yellow
+Start-Sleep 5
+stop-process -Id $PID
---
+++ zabbix_agent_6.0.sh
@@ -0,0 +1,87 @@
+#  Version:        1.0
+#  Author:         bygalacos
+#  Github:         github.com/bygalacos
+#  Creation Date:  22.12.2022
+#  Purpose/Change: Initial script development
+
+# Check if running as root
+if [[ $(id -u) -ne 0 ]]; then
+  echo "Please run this script as root"
+  exit 1
+fi
+
+# Check if running on CentOS
+if [[ $(grep -Ei 'centos|red hat' /etc/*release) ]]; then
+  clear
+  setenforce 0 && sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
+  
+  # Remove Previous Files
+  yum erase -y zabbix-agent
+  if [ -f /etc/zabbix/zabbix_agentd.conf.rpmsave ]; then
+    rm -rf /etc/zabbix/zabbix_agentd.conf.rpmsave
+  fi
+
+  # Update Package Repository & Install Zabbix Agent
+  yum install -y ca-certificates
+  rpm -Uvh https://repo.zabbix.com/zabbix/6.0/rhel/$(rpm -E %{rhel})/x86_64/zabbix-release-6.0-4.el$(rpm -E %{rhel}).noarch.rpm
+  yum install -y zabbix-agent
+  clear
+
+  # Get IP Address of Zabbix Server
+  if [ -z "$1" ]; then
+    read -p "Enter the IP address of the Zabbix server: " zabbixIP
+  else
+    zabbixIP=$1
+  fi
+  hostname=$(hostname)
+
+  # Replace Server, ServerActive and Hostname in Zabbix Agent configuration file
+  sed -i "s/Server=127.0.0.1/Server=$zabbixIP/g" /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/ServerActive=127.0.0.1/ServerActive=$zabbixIP/g" /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/Hostname=Zabbix server/Hostname=$hostname/g" /etc/zabbix/zabbix_agentd.conf
+
+  # Restart Zabbix Agent
+  systemctl restart zabbix-agent && systemctl enable zabbix-agent
+  clear
+  systemctl status zabbix-agent
+  exit 0
+fi
+
+# Check if running on Ubuntu
+if [[ $(grep -Ei 'ubuntu' /etc/*release) ]]; then
+  clear
+  
+  # Remove Previous Files
+  apt-get purge -y zabbix-agent
+
+  # Update Package Repository & Install Zabbix Agent
+  apt-get update
+  apt-get install -y ca-certificates
+  wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu$(lsb_release -rs)_all.deb
+  dpkg -i zabbix-release_* && rm -rf zabbix-release_*
+  apt-get update
+  apt-get install -y zabbix-agent
+  clear
+
+  # Get IP Address of Zabbix Server
+  if [ -z "$1" ]; then
+    read -p "Enter the IP address of the Zabbix server: " zabbixIP
+  else
+    zabbixIP=$1
+  fi
+  hostname=$(hostname)
+
+  # Replace Server, ServerActive and Hostname in Zabbix Agent configuration file
+  sed -i "s/Server=127.0.0.1/Server=$zabbixIP/g" /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/ServerActive=127.0.0.1/ServerActive=$zabbixIP/g" /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/Hostname=Zabbix server/Hostname=$hostname/g" /etc/zabbix/zabbix_agentd.conf
+
+  # Restart Zabbix Agent
+  systemctl restart zabbix-agent && systemctl enable zabbix-agent
+  clear
+  systemctl status zabbix-agent
+  exit 0
+fi
+
+  echo "Unsupported operating system"
+  exit 1
